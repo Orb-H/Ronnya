cad77103ced6760c9bb43d66513b62fddcb331ad
Jongryeol Han <orb.cov@gmail.com>
2023-02-16 17:42:19 +0900

    Update getToken.py
    Change type of parameters of GetToken and remove redundant code

--- modules/getToken.py
+++ modules/getToken.py
@@ -1,19 +1,17 @@
 import requests
 import json
     
-def GetToken(uid : int,token : int):
+def GetToken(uid : str,token : str):
     """_summary_
 
     Args:
-        uid (int): uid of your account
+        uid (str): uid of your account
         token (str): token of your account
 
     Returns:
         Dict: Dict of response. accessToken is in returns["accessToken"]
     """
-    uid = str(uid);
-    token = token;
-    devicdId = "web|"+str(uid);
+    devicdId = "web|"+uid;
     url="https://passport.mahjongsoul.com/user/login";
     data={
         "uid": uid,
