acece4888f5d2ed505b052768e7b325f188f709e
Orb-H <orb.cov@gmail.com>
2023-02-27 11:57:38 +0900

    Add websocket library

---
+++ modules/lq_proto_util.py
@@ -0,0 +1,143 @@
+import lq_proto_pb2
+import json
+
+lq = json.loads(''.join(open('data/lq_proto.json').readlines()))['nested']
+lq = lq['lq']['nested']
+fasttest = lq['FastTest']
+lobby = lq['Lobby']
+
+def get_types(type: str) -> list[str]:
+    '''
+    Args:
+        type (str): Type of wrapper without prefix
+    
+    Returns:
+        list[str]: Type of wrapper, request type of message, response type of message
+    '''
+    for k, v in lobby['methods'].items():
+        if k == type:
+            return '.lq.Lobby.' + type, v['requestType'], v['responseType']
+    for k, v in fasttest['methods'].items():
+        if k == type:
+            return '.lq.FastTest.' + type, v['requestType'], v['responseType']
+    return None
+
+def to_dict(message: bytes, n: int = 0) -> dict:
+    '''
+    Args:
+        msg (bytes): Serialized message
+        n (int): Indent number for printing, not used
+    
+    Returns:
+        dict: Deserialized content of message
+    '''
+    message_dict = {}
+    
+    for descriptor in message.DESCRIPTOR.fields:
+        key = descriptor.name
+        value = getattr(message, descriptor.name)
+        # print('  ' * n + key, end='\t')
+        
+        if descriptor.label == descriptor.LABEL_REPEATED:
+            message_list = []
+            
+            for sub_message in value:
+                if descriptor.type == descriptor.TYPE_MESSAGE:
+                    # print()
+                    message_list.append(to_dict(sub_message, n + 1))
+                    message_list[-1]['_type_'] = sub_message.DESCRIPTOR.name
+                elif descriptor.type == descriptor.TYPE_BYTES:
+                    # print()
+                    if len(value) != 0:
+                        wrapper = lq_proto_pb2.Wrapper()
+                        wrapper.ParseFromString(value)
+                        # print(wrapper.name, end='\t')
+                        try:
+                            d = getattr(lq_proto_pb2, wrapper.name.split('.')[-1])()
+                            d.ParseFromString(wrapper.data)
+                            message_dict[key] = to_dict(d, n + 1)
+                            message_dict[key]['_type_'] = wrapper.name
+                        except:
+                            message_dict[key] = str(value)
+                else:
+                    # print(sub_message, end='\t')
+                    message_list.append(sub_message)
+            
+            # print()
+            message_dict[key] = message_list
+        else:
+            if descriptor.type == descriptor.TYPE_MESSAGE:
+                # print()
+                message_dict[key] = to_dict(value, n + 1)
+                message_dict[key]['_type_'] = value.DESCRIPTOR.name
+            elif descriptor.type == descriptor.TYPE_BYTES:
+                if len(value) != 0:
+                    wrapper = lq_proto_pb2.Wrapper()
+                    wrapper.ParseFromString(value)
+                    # print(wrapper.name, end='\t')
+                    try:
+                        d = getattr(lq_proto_pb2, wrapper.name.split('.')[-1])()
+                        d.ParseFromString(wrapper.data)
+                        message_dict[key] = to_dict(d, n + 1)
+                        message_dict[key]['_type_'] = wrapper.name
+                    except:
+                        message_dict[key] = str(value)
+            else:
+                # print(value)
+                message_dict[key] = value
+    
+    return message_dict
+
+def wrap_root_msg(msg: bytes, type: str, idx: int) -> bytes:
+    '''
+    Args:
+        msg (bytes): Serialized message
+        type (str): Type of wrapper
+        idx (int): Index number of this message
+    
+    Returns:
+        bytes: Wrapped message. This is ready to be sent to the server.
+    '''
+    wrapper = lq_proto_pb2.Wrapper()
+    wrapper.name = type
+    wrapper.data = msg
+    res = bytes([2, idx & 0xff, idx >> 8]) + wrapper.SerializeToString()
+    return res
+
+def wrap_msg(msg: dict, reqtype: str) -> bytes:
+    '''
+    Args:
+        msg (dict): content of message to be sent
+        reqtype (str): Request type of message
+    
+    Returns:
+        bytes: Serialized message.
+    '''
+    Target = getattr(lq_proto_pb2, reqtype)
+    wrapper = Target(**msg)
+    return wrapper.SerializeToString()
+
+def unwrap_root_msg(msg: bytes) -> bytes:
+    '''
+    Args:
+        msg (bytes): Message received from the server
+    
+    Returns:
+        bytes: Unwrapped message of the message. Should be deserialized to use.
+    '''
+    wrapper = lq_proto_pb2.Wrapper()
+    wrapper.ParseFromString(msg[3:])
+    return wrapper.data
+
+def unwrap_msg(msg: bytes, restype: str) -> dict:
+    '''
+    Args:
+        msg (bytes): Serialized message
+    
+    Returns:
+        dict: Deserialized content of the message.
+    '''
+    Target = getattr(lq_proto_pb2, restype)
+    wrapper = Target()
+    wrapper.ParseFromString(msg)
+    return to_dict(wrapper, 0)
---
+++ modules/web_socket_client.py
@@ -0,0 +1,169 @@
+import asyncio
+import websockets
+import lq_proto_util
+import lq_proto_pb2
+import os
+import uuid
+
+import getData
+
+class WebSocketClient:
+    def __init__(self):
+        self.index = 1
+    
+    async def connect(self):
+        self.client = await websockets.connect(os.getenv('ws_server_addr_jp'))
+        return await self.send_heatbeat()
+    
+    async def send_msg(self, msg: bytes, type: str) -> bytes:
+        wrapped = lq_proto_util.wrap_root_msg(msg, type, self.index)
+        self.index += 1
+        await self.client.send(wrapped)
+
+        resp = await self.client.recv()
+        # print(resp)
+        unwrapped = lq_proto_util.unwrap_root_msg(resp)
+        # print(unwrapped)
+        return unwrapped
+    
+    async def send_type_msg(self, msg: dict, type: str) -> bytes:
+        types = lq_proto_util.get_types(type)
+        msg = lq_proto_util.wrap_msg(msg, types[1])
+        resp = await self.send_msg(msg, types[0])
+        if len(resp) > 0:
+            return lq_proto_util.unwrap_msg(resp, types[2])
+        return ''
+
+    async def send_heatbeat(self):
+        return await self.send_type_msg({
+            'no_operation_counter': 0
+        }, 'heatbeat')
+
+    async def send_oauth2auth_msg(self, login_type: int, code: str, uid: str, version_str: str) -> dict:
+        return await self.send_type_msg({
+            'type': login_type,
+            'code': code,
+            'uid': uid,
+            'client_version_string': version_str
+        }, 'oauth2Auth')
+
+    async def send_oauth2check_msg(self, login_type: int, access_token: str) -> dict:
+        return await self.send_type_msg({
+            'type': login_type,
+            'access_token': access_token
+        }, 'oauth2Check')
+    
+    async def send_oauth2login_msg(self, login_type: int, access_token: str, version: str, version_str: str):
+        deviceInfo = lq_proto_pb2.ClientDeviceInfo(**{
+            'platform': 'pc',
+            'hardware': 'pc',
+            'os': 'windows',
+            'os_version': 'win10',
+            'is_browser': True,
+            'software': 'Chrome',
+            'sale_platform': 'web'
+        })
+        versionInfo = lq_proto_pb2.ClientVersionInfo(**{
+            'resource': version
+        })
+        request = lq_proto_pb2.ReqOauth2Login(**{
+            'type': login_type,
+            'access_token': access_token,
+            'reconnect': False,
+            'device': deviceInfo,
+            'random_key': str(uuid.uuid4()),
+            'client_version': versionInfo,
+            'client_version_string': version_str,
+            'currency_platforms': []
+        })
+        response = await self.send_msg(request.SerializeToString(), '.lq.Lobby.oauth2Login')
+        return lq_proto_util.unwrap_msg(response, 'ResLogin')
+
+    async def login(self, code: str, uid: str, login_type: int, version: str, version_str: str) -> bool:
+        # LOGIN: OAuth2Auth -> OAuth2Check -> OAuth2Login
+        print('Send Oauth2Auth')
+        response = await self.send_oauth2auth_msg(8, code, uid, version_str)
+        assert 'access_token' in response.keys(), 'Oauth2Auth Failed'
+        access_token = response['access_token']
+        print('Done Oauth2Auth: access_token = ' + access_token)
+        
+        print('Send Oauth2Check')
+        response = await self.send_oauth2check_msg(login_type, access_token)
+        assert 'has_account' in response.keys(), 'Oauth2Check Failed'
+        has_account = response['has_account']
+        print('Done Oauth2Check: has_account = ' + str(has_account))
+        assert has_account, ('No account found with uid ' + uid)
+
+        print('Send Oauth2Login')
+        response = await self.send_oauth2login_msg(login_type, access_token, version, version_str)
+        assert 'account_id' in response.keys(), 'Oauth2Login Failed'
+        account_id = response['account_id']
+    
+    async def send_searchaccountbypattern_msg(self, fid: str) -> dict:
+        return await self.send_type_msg({
+            'search_next': False,
+            'pattern': fid
+        }, 'searchAccountByPattern')
+
+    async def send_fetchmultiaccountbrief_msg(self, uid: str) -> dict:
+        request = lq_proto_pb2.ReqMultiAccountId(**{
+            'account_id_list': [uid]
+        })
+        response = await self.send_msg(request.SerializeToString(), '.lq.Lobby.fetchMultiAccountBrief')
+        return lq_proto_util.unwrap_msg(response, 'ResMultiAccountBrief')
+    
+    async def find_user(self, fid: str) -> dict:
+        # INFO_QUERY: searchAccountByPattern -> fetchMultiAccountBrief
+        print('Send SearchAccountByPattern')
+        response = await self.send_searchaccountbypattern_msg(fid)
+        assert 'decode_id' in response, 'No account found'
+        uid = response['decode_id']
+        print('Done SearchAccountByPattern: decode_id = ' + str(uid))
+
+        print('Send FetchMultiAccountBrief')
+        response = await self.send_fetchmultiaccountbrief_msg(uid)
+        assert 'players' in response, 'No user information found'
+        print('Done FetchMultiAccountBrief: user information ↓')
+        print(response['players'])
+        return response['players']
+
+    async def close(self):
+        return await self.client.close()
+
+async def main():
+    print('----- Manual Mode -----')
+    try:
+        f = open('.ws_client.sock', 'w')
+
+        uid = os.getenv('ronnya_uid')
+        token = os.getenv('ronnya_token')
+        access_token = getData.GetToken(uid, token)['accessToken']
+
+        version = getData.GetVersion()['version']
+        version_str = 'web-' + '.'.join(version.split('.')[:-1])
+        
+        client = WebSocketClient()
+        print('Intialize connection')
+        resp = await client.connect()
+        assert len(resp) == 0, "Error at heatbeat: " + resp
+        print('First heatbeat complete')
+
+        print('Attempt to login')
+        await client.login(access_token, uid, 8, version, version_str)
+
+        while True:
+            fid = input('> ')
+            if len(fid) == 0:
+                break
+            print((await client.find_user(fid))[0]['nickname'])
+        await client.close()
+    except Exception as e:
+        print(type(e))
+        print(e.args)
+        print(e)
+    finally:
+        f.close()
+        os.remove('.ws_client.sock')
+
+if __name__ == "__main__":
+    asyncio.run(main())
